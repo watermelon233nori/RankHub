044a6cfd2f130d980382157830ea1666190a0f3f
qianmo2233 <67282958@qq.com>
2025-11-17 00:53:20 +0800

    refactor

--- lib/controllers/account_controller.dart
+++ lib/controllers/account_controller.dart
@@ -2,7 +2,6 @@ import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:rank_hub/models/account/account.dart';
 import 'package:rank_hub/services/account_service.dart';
-import 'package:rank_hub/services/providers/diving_fish_credential_provider.dart';
 import 'package:rank_hub/modules/maimai_lxns/services/lxns_credential_provider.dart';
 import 'package:rank_hub/services/platform_login_manager.dart';
 
@@ -33,7 +32,6 @@ class AccountController extends GetxController {
     PlatformLoginManager.instance.initialize();
 
     // 注册凭据提供者
-    _accountService.registerProvider(DivingFishCredentialProvider());
     _accountService.registerProvider(LxnsCredentialProvider());
 
     // 加载账号列表
