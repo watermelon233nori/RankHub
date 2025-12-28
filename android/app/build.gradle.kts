aff8ac7767fedfd508f42dff9a1042c3d71f6440
qianmo2233 <67282958@qq.com>
2025-12-28 21:53:02 +0800

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     defaultConfig {
@@ -40,7 +43,7 @@ android {
         versionName = flutter.versionName
 
         ndk {
-            abiFilters "arm64-v8a"
+            abiFilters += listOf("arm64-v8a")
         }
     }
 
