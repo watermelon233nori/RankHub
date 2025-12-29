228a8550758be0d479cb50a57957e0bd8847bb1f
qianmo2233 <67282958@qq.com>
2025-12-29 10:11:48 +0800

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,10 +41,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
-        ndk {
-            abiFilters += listOf("arm64-v8a")
-        }
     }
 
     signingConfigs {
